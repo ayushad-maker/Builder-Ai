b6fb8beaf84f788e98f3115ad94a14ebd61bb152
Ayush Adhikari <ayushadhikari080@gmail.com>
2026-08-05 12:59:26 +0530

    Implement authentication pages with login and registration functionality; add loading component and context for user state management

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,5 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import api from "../api/api";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const AppContext = createContext(undefined);
 
@@ -7,26 +9,55 @@ export function AppContextProvider({ children }) {
   //Auth States
   const [user, setUser] = useState(null);
   const [loadingUser, setLoadingUser] = useState(true);
+  const navigate = useNavigate();
 
   //check session
 
   const checkSessions = async () => {
     try {
       const { data } = await api.get("/api/auth/me");
-      // setUser(data.user);
+      setUser(data.user);
     } catch (error) {
       setUser(null);
-    }finally{
+    } finally {
       setLoadingUser(false);
     }
   };
 
-  useEffect(()=>{
-    checkSessions(); 
-  },[checkSessions])
-   
+  useEffect(() => {
+    checkSessions();
+  }, []);
+
+  const Login = async (email, password) => {
+    try {
+      const { data } = await api.post("/api/auth/login", { email, password });
+      setUser(data.user);
+      toast.success("Login Successful");
+      navigate("/");
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Login Failed");
+      setUser(null);
+    }
+  };
+
+  const Register = async (name, email, password) => {
+    try {
+      const { data } = await api.post("/api/auth/register", {
+        name,
+        email,
+        password,
+      });
+      setUser(data.user);
+      toast.success("Registration Successful");
+      navigate("/");
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Login Failed");
+      setUser(null);
+    }
+  };
+
   return (
-    <AppContext.Provider value={{ user, loadingUser }}>
+    <AppContext.Provider value={{ user, loadingUser, Login, Register }}>
       {children}
     </AppContext.Provider>
   );
--- client/src/pages/AuthPages.jsx
+++ client/src/pages/AuthPages.jsx
@@ -1,9 +1,169 @@
+import { useState } from "react";
+import LoginLeft from "../components/LoginLeft";
+import { Link } from "react-router-dom";
+import { EyeIcon, EyeOffIcon, Loader2Icon } from "lucide-react";
+import { useAppContext } from "../context/AppContext";
+import { useNavigate } from "react-router-dom";
 
+const Authpage = ({ mode }) => {
+  const { Login, Register } = useAppContext();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
 
-const Authpages = () => {
+  const isLogin = mode === "login";
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError("");
+
+    try {
+      if (isLogin) {
+        await Login(email, password);
+      } else {
+        await Register(name, email, password);
+      }
+
+      navigate("/");
+    } catch (error) {
+      setError(
+        error.message ||
+          (isLogin ? "Invalid email or password" : "Registration failed"),
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>Authpages</div>
-  )
-}
+    <div className="min-h-screen bg-white flex text-zinc-900 font-sans">
+      {/* Left Page */}
+      <LoginLeft />
 
-export default Authpages
+      {/* Right Page */}
+      <div className="flex-1 flex items-center justify-center p-8">
+        <div className="w-full max-w-sm">
+          <div className="mb-10">
+            <h1 className="text-3xl font-medium tracking-tight text-zinc-900 mb-1.5 font-sans">
+              {isLogin ? "Sign in " : "Create an account"}
+            </h1>
+            <p className="text-sm text-zinc-400">
+              {isLogin
+                ? "Enter your credentials to access your website builder"
+                : "Get started by entering your registration detail"}
+            </p>
+          </div>
+
+          {error && (
+            <div className="mb-6 p-3 border-red-200 bg-red-50 text-red-700 text-xs rounded">
+              {error}
+            </div>
+          )}
+
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            {!isLogin && (
+              <div>
+                <label className="block text-[11px] font-semibold text-zinc-400 uppercase tracking-widest mb-2">
+                  Full Name
+                </label>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="w-full pl-2 py-2 border-b border-zinc-200 focus:outline-none focus:border-zinc-950 text-sm"
+                  placeholder="John Doe"
+                  required
+                />
+              </div>
+            )}
+
+            <div>
+              <label className="block text-[11px] font-semibold text-zinc-400 uppercase tracking-widest mb-2 mt-3">
+                Email Address
+              </label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full pl-2 py-2 border-b border-zinc-200 focus:outline-none focus:border-zinc-950 text-sm"
+                placeholder="example@gmail.com"
+                required
+              />
+            </div>
+
+            <div>
+              <label className="block text-[11px] font-semibold text-zinc-400 uppercase tracking-widest mb-2 mt-3">
+                Password
+              </label>
+
+              <div className="relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full pl-2 py-2 border-b border-zinc-200 focus:outline-none focus:border-zinc-950 text-sm"
+                  placeholder="••••••••"
+                  required
+                />
+
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-2 top-1/2 -translate-y-1/2 text-zinc-400 hover:text-zinc-700"
+                >
+                  {showPassword ? (
+                    <EyeOffIcon size={16} />
+                  ) : (
+                    <EyeIcon size={16} />
+                  )}
+                </button>
+              </div>
+            </div>
+
+            <button
+              type="submit"
+              disabled={loading}
+              className="w-full py-2.5 bg-linear-to-br from-red-600 to-amber-600 text-white font-semibold hover:scale-102 disabled:opacity-40 flex items-center justify-center cursor-pointer mt-2 rounded-lg transition-all"
+            >
+              {loading && (
+                <Loader2Icon className="animate-spin h-3.5 w-3.5 mr-2" />
+              )}
+              {isLogin ? " Sign In" : "Sign Up"}
+            </button>
+          </form>
+
+          <p className="text-sm text-zinc-900 mt-8 pt-6 border-t border-zinc-900 font-sans">
+            {isLogin ? (
+              <>
+                New To BuilderAI?{" "}
+                <Link
+                  to="/register"
+                  className="text-zinc-900 font-medium hover:underline"
+                >
+                  Create an account
+                </Link>
+              </>
+            ) : (
+              <>
+                Already have an account?{" "}
+                <Link
+                  to="/login"
+                  className="text-zinc-900 font-medium hover:underline"
+                >
+                  Sign in here
+                </Link>
+              </>
+            )}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Authpage;
